719edebbfa7ba6c5d4a02974b8ae676d20791ec7
Rinzlo <joshuarco@gmail.com>
2018-10-22 02:45:12 -0700

    dynamic routing with regex

--- Core/Router.php
+++ Core/Router.php
@@ -16,15 +16,24 @@ class Router
     protected $params = [];
 
     /**
-     * Add a route
+     * Adds a route
      *
      * @param string $route
-     * @param $params
+     * @param array $params
      *
      * @return void
      */
-    public function add(string $route, $params): void
+    public function add(string $route, $params = []): void
     {
+        // Convert the route to a regular expression: escape forward slashes
+        $route = preg_replace('/\//', '\\/', $route);
+
+        // Convert variables e.g. {controller}
+        $route = preg_replace('/\{([a-z]+)\}/', '(?P<\1>[a-z-]+)', $route);
+
+        // Add start and end delimiters, and case insensitive flag
+        $route = '/^' . $route . '$/i';
+
         $this->routes[$route] = $params;
     }
 
@@ -46,26 +55,22 @@ class Router
      */
     public function match(string $url): bool
     {
-//        foreach($this->routes as $route => $params){
-//            if($url == $route){
-//                $this->params = $params;
-//                return true;
-//            }
-//        }
-//        return false;
+        // Match to the fixed URL format /controller/action
+        //$reg_exp = "/^(?<controller>[a-z-]+)\/(?<action>[a-z-]+)$/";
 
-        $exp = "/^(?<controller>[a-z-]+)\/(?<action>[a-z-]+)$/";
+        foreach ($this->routes as $route => $params){
+            if(preg_match($route, $url, $matches)){
+                // Get named capture group values
+                //$params = [];
 
-        if(preg_match($exp, $url, $matches)){
-            $params = [];
-
-            foreach ($matches as $key => $match){
-                if(is_string($key)){
-                    $params[$key] = $match;
+                foreach ($matches as $key => $match){
+                    if(is_string($key)){
+                        $params[$key] = $match;
+                    }
                 }
+                $this->params = $params;
+                return true;
             }
-            $this->params = $params;
-            return true;
         }
         return false;
     }
