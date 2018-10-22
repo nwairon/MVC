4b15ee7677b1b6a171792c9b638b73ee207e53e3
Rinzlo <joshuarco@gmail.com>
2018-10-21 23:52:07 -0700

    adding directories

--- Core/Router.php
+++ Core/Router.php
@@ -3,5 +3,63 @@ declare(strict_types=1);
 
 class Router
 {
+    /**
+     * Routing Table
+     * @var array
+     */
+    protected $routes = [];
 
+    /**
+     * Parameters
+     * @var array
+     */
+    protected $params = [];
+
+    /**
+     * Add a route
+     *
+     * @param string $route
+     * @param $params
+     *
+     * @return void
+     */
+    public function add(string $route, $params): void
+    {
+        $this->routes[$route] = $params;
+    }
+
+    /**
+     * Get all routes
+     *
+     * @return array
+     */
+    public function getRoutes(): array
+    {
+        return $this->routes;
+    }
+
+    /**
+     * Match the route to the table
+     *
+     * @param string $url
+     * @return bool
+     */
+    public function match(string $url): bool
+    {
+        foreach($this->routes as $route => $params){
+            if($url == $route){
+                $this->params = $params;
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * @return array
+     */
+    public function getParams(): array
+    {
+        return $this->params;
+    }
 }
--- public/index.php
+++ public/index.php
@@ -11,4 +11,25 @@ require '../Core/Router.php';
 
 $router = new Router();
 
-echo get_class($router);
+//echo get_class($router);
+
+// Add the routes
+$router->add('', ['controller' => 'Home', 'action' => 'index']);
+$router->add('posts', ['controller' => 'Posts', 'action' => 'index']);
+$router->add('posts/new', ['controller' => 'Posts', 'action' => 'new']);
+
+// Display the routing table
+//echo '<pre>';
+//var_dump($router->getRoutes());
+//echo '</pre>';
+
+// Match requested route
+$url = $_SERVER['QUERY_STRING'];
+
+if($router->match($url)){
+    echo '<pre>';
+    var_dump($router->getParams());
+    echo '</pre>';
+}else{
+    echo "No route found for URL '$url'";
+}
